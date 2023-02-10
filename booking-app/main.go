460feaef53212bb2081985b8b6e7449d8671ab57
Trey Everson <teverson25@me.com>
2023-02-09 20:12:09 -0600

    Adding variables and print formatting

--- booking-app/main.go
+++ booking-app/main.go
@@ -2,7 +2,16 @@ package main
 
 import "fmt"
 
-func main(){
-	fmt.Print("Hello World")
-}
+func main() {
 
+	// Defining a variable
+	var conferenceName = "Go conference"
+	// Constants do not change
+	const conferenceTickets = 50
+	var remainingTickets = 50
+
+	// fmt.Println("Welcome to the", conferenceName, "booking application\n Buy your tickets here.")
+	fmt.Printf("Welcome to the %v booking application\n", conferenceName)
+	fmt.Printf("We have a total of %v and are %v available.", conferenceTickets, remainingTickets)
+
+}
